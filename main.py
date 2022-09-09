913f10e65b99fce5e5a57b94c9736dfa8726ffb6
PUSHKAR RAJ <pushkar.raj6@gmail.com>
2022-09-10 02:42:21 +0530

    requirements.txt changed

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import pandas as pd
-from youtube_scrapper_test import YoutubeScrapper
-
-
-
-
-
-#Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    driver_loc = r"C:\Users\dnsingh\Downloads\Compressed\chromedriver_win32\chromedriver.exe"
-    url = "https://www.youtube.com/c/HiteshChoudharydotcom/videos"
-    n=2
-    scrape = YoutubeScrapper(url,n,driver_loc)
-    scrape.final_process()
+# import pandas as pd
+# from youtube_scrapper_test import YoutubeScrapper
+#
+#
+#
+#
+#
+# #Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     driver_loc = r"C:\Users\dnsingh\Downloads\Compressed\chromedriver_win32\chromedriver.exe"
+#     url = "https://www.youtube.com/c/HiteshChoudharydotcom/videos"
+#     n=2
+#     scrape = YoutubeScrapper(url,n,driver_loc)
+#     scrape.final_process()
 
 #https://www.youtube.com/user/krishnaik06/videos
 #https://www.youtube.com/user/saurabhexponent1/videos
